17d3dfc51893b455f2b84883a1677e4123f6549c
KNU-K <knukang334@gmail.com>
2024-05-09 12:30:54 +0900

    [feat/chat] api 구성 및 service/controller 구성

--- api/index.js
+++ api/index.js
@@ -5,13 +5,15 @@ const auth = require("./auth/index");
 const board = require("./board/index");
 // const search = require("./search/index");
 const comment = require("./comment/index");
+const chat = require("./chat/index");
 router.use("/user", user);
 router.use("/auth", auth);
 router.use("/board", board);
 router.use("/comment", comment);
+router.use("/chat", chat);
 router.get("/test", (req, res, next) => {
   res.json(req.user);
 });
+
 // router.use("/search", search);
-// router.use("/follow", follow);
 module.exports = router;
